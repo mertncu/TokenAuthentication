11b1e5b6efe319f789368c3e9b5dfa12889188aa
mertncu <ymuncu@gmail.com>
2024-08-03 02:11:25 +0300

    Add refreshToken to LoginResponse model

--- app/src/main/java/com/eosdev/tokenauthentication/models/LoginResponse.kt
+++ app/src/main/java/com/eosdev/tokenauthentication/models/LoginResponse.kt
@@ -3,7 +3,6 @@ package com.eosdev.tokenauthentication.models
 import com.google.gson.annotations.SerializedName
 
 data class LoginResponse(
-    @SerializedName("token")
-    val token: String? = null,
+    val token: String,
+    val refreshToken: String
 )
-
