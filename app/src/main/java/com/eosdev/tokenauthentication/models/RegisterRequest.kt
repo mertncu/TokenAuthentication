5a1d8e628bf84c5b56a0d22736b5b0e5ceb8a1a3
mertncu <ymuncu@gmail.com>
2024-08-03 03:10:36 +0300

    model regularization

--- app/src/main/java/com/eosdev/tokenauthentication/models/RegisterRequest.kt
+++ app/src/main/java/com/eosdev/tokenauthentication/models/RegisterRequest.kt
@@ -3,12 +3,14 @@ package com.eosdev.tokenauthentication.models
 import com.google.gson.annotations.SerializedName
 
 data class RegisterRequest(
+
     @SerializedName("userName")
     val userName: String? = null,
+    @SerializedName("userSurname")
+    val userSurname: String? = null,
     @SerializedName("Email")
     val email: String? = null,
     @SerializedName("Password")
-    val password: String? = null,
-    @SerializedName("userSurname")
-    val userSurname: String? = null
+    val password: String? = null
+
 )
--- app/src/main/java/com/eosdev/tokenauthentication/models/User.kt
+++ app/src/main/java/com/eosdev/tokenauthentication/models/User.kt
@@ -3,14 +3,8 @@ package com.eosdev.tokenauthentication.models
 import com.google.gson.annotations.SerializedName
 
 data class User(
-    @SerializedName("userName")
-    val userName: String? = null,
-    @SerializedName("userId")
-    val userId: Int? = null,
     @SerializedName("Email")
     val email: String? = null,
     @SerializedName("Password")
-    val password: Int? = null,
-    @SerializedName("userSurname")
-    val userSurname: String? = null
+    val password: String? = null,
 )
