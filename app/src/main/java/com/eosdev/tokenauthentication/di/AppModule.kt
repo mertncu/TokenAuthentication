c9065473c694875f20ff3121b6c413c3f3b26490
mertncu <ymuncu@gmail.com>
2024-08-03 03:08:37 +0300

    Update Dagger Hilt Module

--- app/src/main/java/com/eosdev/tokenauthentication/di/AppModule.kt
+++ app/src/main/java/com/eosdev/tokenauthentication/di/AppModule.kt
@@ -1,10 +1,15 @@
 package com.eosdev.tokenauthentication.di
 
+import android.content.Context
 import com.eosdev.tokenauthentication.service.auth.AuthApiService
 import com.eosdev.tokenauthentication.utils.AuthInterceptor
+import com.eosdev.tokenauthentication.utils.TokenAuthenticator
+import com.eosdev.tokenauthentication.utils.TokenDataStore
+import com.eosdev.tokenauthentication.utils.TokenManager
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -15,6 +20,24 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideTokenDataStore(@ApplicationContext context: Context): TokenDataStore {
+        return TokenDataStore(context)
+    }
+
+    @Provides
+    @Singleton
+    fun provideTokenManager(tokenDataStore: TokenDataStore): TokenManager {
+        return TokenManager(tokenDataStore)
+    }
+
+    @Provides
+    @Singleton
+    fun provideAuthInterceptor(tokenManager: TokenManager): AuthInterceptor {
+        return AuthInterceptor(tokenManager)
+    }
 
     @Provides
     @Singleton
@@ -44,5 +67,14 @@ object AppModule {
     @Singleton
     fun provideApiService(retrofit: Retrofit): AuthApiService {
         return retrofit.create(AuthApiService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideTokenAuthenticator(
+        tokenManager: TokenManager,
+        apiService: AuthApiService
+    ): TokenAuthenticator {
+        return TokenAuthenticator(tokenManager, apiService)
     }
 }
